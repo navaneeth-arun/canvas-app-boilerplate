36ca1db0b6fb24c107c774cbe1db4ef5793c4b55
unknown <navaarun18@gmail.com>
2022-09-13 18:16:21 +0530

    text

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 /* Iteration 4: Make the Everything Work */
+// <!-- Ronaldo - background, jersey, face, hair, cr, number -->
+const background = document.getElementById("background");
+const buttonjersey = document.getElementById("buttonjersey");
+const buttonface = document.getElementById("buttonface");
+const buttonhair = document.getElementById("buttonhair");
+const buttoncr = document.getElementById("buttoncr");
+const buttonnumber = document.getElementById("buttonnumber");
+
+buttonbackground.oninput=()=>{
+  background.style.fill=buttonbackground.value;
+}
+buttonjersey.oninput=()=>{
+  jersey.style.fill=buttonjersey.value;
+}
+buttonface.oninput=()=>{
+  face.style.fill=buttonface.value;
+}
+buttonhair.oninput=()=>{
+  hair.style.fill=buttonhair.value;
+}
+buttoncr.oninput=()=>{
+  cr.style.fill=buttoncr.value;
+}
+buttonnumber.oninput=()=>{
+  number.style.fill=buttonnumber.value;
+}
